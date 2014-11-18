5ac1ccc07065e25b7e48839d893164dd0412d0c6
Diego Lorden <djlorden@gmail.com>
2014-11-18 06:47:36 -0800

    Test for OpenShift

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='SoftwareOfThings.io',
+      version='1.0',
+      description='Software of Things website',
+      author='Diego Lorden',
+      url='http://www.python.org/sigs/distutils-sig/',
+     )
